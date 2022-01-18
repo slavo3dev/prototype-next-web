5a9ab553ae93ef2333831771a6a5d1400334b16e
slavo3dev <slavo@slavo3.com>
2022-01-17 20:06:55 -0500

    adding schema

--- s3studio/schemas/schema.js
+++ s3studio/schemas/schema.js
@@ -11,6 +11,32 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      name: "blog",
+      type: "document",
+      title: "Blog",
+      fields: [
+      {
+        name: 'title', 
+        type: 'string',
+        title: 'Title'
+      },
+      {
+        name: 'subtitle', 
+        type: 'string',
+        title: 'Subtitle'
+        },
+      {
+        name: 'isFeatured', 
+        type: 'boolean',
+        title: 'isFeatured'
+      },
+      {
+        name: 'slug', 
+        type: 'slug',
+        title: 'Slug'
+      },
+      ]
+    }
   ]),
 })
