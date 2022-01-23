8bf76a69d0650da331f65cab8230d668f4c04103
slavo3dev <slavo@slavo3.com>
2022-01-22 22:55:59 -0500

    importing blogFields

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,19 @@
+import Blog from "pages/blog";
 import client from "./sanity"
 
 
 export async function getAllPosts ()
 {
+
+    const blogFields =`
+    title, 
+    subtitle,
+    isFeatured, 
+    slug
+    `
+
    const blogData = await client
-       .fetch( `*[_type == "blog"]{title, subtitle, slug, isFeatured}` );
+       .fetch( `*[_type == "blog"]{${blogFields}}` );
     
     return blogData;
 }
