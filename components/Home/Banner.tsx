b0917b96441f507884ac9540426f08f44e35c7ce
Slavo Popovic <slavo@slavo3.com>
2022-07-22 12:01:05 -0400

    adding webp image

--- components/Home/Banner.tsx
+++ components/Home/Banner.tsx
@@ -63,7 +63,7 @@ const Banner = () =>
             </div>
             <div className='col-xl-7 col-lg-6 col-md-12 p-0'>
               <div className='banner-wrapper-image'>
-             <Image src='/images/banner/banner-img3.webp' alt='Prototype.NEXT - Image' width={ 600 } height={500} />
+             <Image src='/images/banner/banner-img5.webp' alt='Prototype.NEXT - Image' width={ 600 } height={500} />
               </div>
             {state === "Error" &&
                  alert("Oops Something went WORONG \nPlease Try Again or You are already a member !!!")}       
