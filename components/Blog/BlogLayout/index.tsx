f7ccfb59d9172bd1d43aece9184aeeaa66c90c7c
slavo3dev <slavo@slavo3.com>
2022-11-28 11:18:55 -0500

    update props

--- components/Blog/BlogLayout/index.tsx
+++ components/Blog/BlogLayout/index.tsx
@@ -1,7 +1,7 @@
 
 import { Container } from "react-bootstrap"
 
-export const BlogLayout: React.FC = ( { children}) =>
+export const BlogLayout: React.FC = ({children}: any) =>
 {
     return (
         <Container style={{marginTop: "10px"}}>
